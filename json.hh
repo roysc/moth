623ebdbb469df1652e0ff178715a16f00e0ae94d
Roy Crihfield <rscrihf@gmail.com>
2014-03-08 18:03:45 -0600

    oops, add json.hh

---
+++ json.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace json = boost::property_tree;
+
+using Json = boost::property_tree::ptree;
+
